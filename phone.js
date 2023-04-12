9d46bdd33b793e4f6d7ef4b3cfc2cc0bb1bcee2d
olaoluwa <olaoluwamichael01@gmail.com>
2023-04-12 12:01:46 +0100

    mickey sign up page

---
+++ phone.js
@@ -0,0 +1,11 @@
+document.getElementById("call").style.display = "none"
+function gallery1(){
+document.getElementById("galleryline").style.display = "none"
+document.getElementById("call").style.display = "block"
+
+}
+document.getElementById("camScreen").style.display = "none"
+function startCam(){
+ document.getElementById('gallcom').style.display = "none"
+document.getElementById("camScreen").style.display = "block"
+}
